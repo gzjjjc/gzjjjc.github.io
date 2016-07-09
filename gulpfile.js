be665104d11000564bd309d13d5096ea71b4ca02
AmosZhong <zhongbm@uclbrt.com>
2016-07-09 15:58:58 +0800

    完成轮转图和菜单

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,43 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var $ = require('gulp-load-plugins')({
-  rename: {
-    'gulp-replace-task': 'replacetask',
-    'gulp-watch' : 'watch'
-  }
+    rename: {
+        'gulp-replace-task': 'replacetask',
+        'gulp-watch': 'watch'
+    }
 });
 var pkg = require('./package.json');
 var AUTOPREFIXER_BROWSERS = [
-    '> 1%',
-    'ie >= 9',
-    'ie_mob >= 9',
-    'Firefox >= 19',
-    'chrome >= 24',
-    'safari >= 6',
-    'opera >= 15',
-    'ios >= 6',
-    'android >= 2.1',
-    'bb >= 10'
+    //
+    // Official browser support policy:
+    // http://v4-alpha.getbootstrap.com/getting-started/browsers-devices/#supported-browsers
+    //
+    'Chrome >= 35', // Exact version number here is kinda arbitrary
+    // Rather than using Autoprefixer's native "Firefox ESR" version specifier string,
+    // we deliberately hardcode the number. This is to avoid unwittingly severely breaking the previous ESR in the event that:
+    // (a) we happen to ship a new Bootstrap release soon after the release of a new ESR,
+    //     such that folks haven't yet had a reasonable amount of time to upgrade; and
+    // (b) the new ESR has unprefixed CSS properties/values whose absence would severely break webpages
+    //     (e.g. `box-sizing`, as opposed to `background: linear-gradient(...)`).
+    //     Since they've been unprefixed, Autoprefixer will stop prefixing them,
+    //     thus causing them to not work in the previous ESR (where the prefixes were required).
+    'Firefox >= 38', // Current Firefox Extended Support Release (ESR); https://www.mozilla.org/en-US/firefox/organizations/faq/
+    // Note: Edge versions in Autoprefixer & Can I Use refer to the EdgeHTML rendering engine version,
+    // NOT the Edge app version shown in Edge's "About" screen.
+    // For example, at the time of writing, Edge 20 on an up-to-date system uses EdgeHTML 12.
+    // See also https://github.com/Fyrd/caniuse/issues/1928
+    'Edge >= 12',
+    'Explorer >= 9',
+    // Out of leniency, we prefix these 1 version further back than the official policy.
+    'iOS >= 8',
+    'Safari >= 8',
+    // The following remain NOT officially supported, but we're lenient and include their prefixes to avoid severely breaking in them.
+    'Android 2.3',
+    'Android >= 4',
+    'Opera >= 12'
 ];
 
-// ***** jianji start ***** //
+// ***** test start ***** //
 var publicRelative = './';
 var publicHome = require(publicRelative + 'index.json');
 
@@ -43,7 +60,7 @@ gulp.task('styles', function() {
             precision: 10,
             onError: console.error.bind(console, 'Sass error:')
         }))
-        .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS,remove: false}))
+        .pipe($.autoprefixer({ browsers: AUTOPREFIXER_BROWSERS, remove: false }))
         .pipe(gulp.dest('.tmp'))
         .pipe($.if('*.css', $.csso()))
         .pipe(gulp.dest(dist))
@@ -108,8 +125,26 @@ gulp.task('images', function() {
 
 });
 
-gulp.task('public', function() {
+gulp.task('public', function(done) {
     runSequence(['styles', 'scripts', 'images']);
+    done();
 });
 
-// ***** public end ***** //
+gulp.task('publicWatch', function (cb) {
+    var scriptsPath = publicHome.js.src;
+    for (var i = 0; i < scriptsPath.length; i++) {
+        scriptsPath[i] = publicRelative + scriptsPath[i];
+    };
+    var stylesPath = publicHome.css.src;
+    for (var i = 0; i < stylesPath.length; i++) {
+        stylesPath[i] = publicRelative + stylesPath[i];
+    };
+
+    scriptsPath = scriptsPath.concat(stylesPath);
+
+    $.watch(scriptsPath, function () {
+        gulp.start('public');
+    });
+});
+
+// ***** test end ***** //
--- js/index.js
+++ js/index.js
@@ -1,8 +1,42 @@
-$(function(){
+$(function() {
     // 图片懒加载
     if ($('img.lazyload-image').lazyload !== undefined) {
         $('img.lazyload-image').lazyload({
             effect: 'fadeIn'
         });
     }
+
+    $("#doc-carousel .inner-item").lazyload({
+        skip_invisible: true,
+    });
+
+    $('#doc-carousel').on('slid.bs.carousel', function() {
+        $("html,body").trigger("scroll");
+    });
+
+    var navbarTransClass = 'navbar-transparent';
+    $(document).bind('scroll', function() {
+        if ($('#doc-header-menu').hasClass('in')) {
+            return;
+        }
+        var len = $(this).scrollTop();
+        if (len >= 58) {
+            $('#doc-header').removeClass(navbarTransClass);
+        } else {
+            $('#doc-header').addClass(navbarTransClass);
+        }
+    });
+
+    $('#doc-header-menu').on('show.bs.collapse', function() {
+        $('#doc-header').removeClass(navbarTransClass);
+    }).on('hide.bs.collapse', function() {
+        var len = $(document).scrollTop();
+        if (len < 58) {
+            $('#doc-header').addClass(navbarTransClass);
+        }
+    });
+
+    $('#doc-header-menu .nav-link').on('click', function() {
+        $('#doc-header-menu').collapse('hide');
+    });
 });
